6a7c9f1a7374cf9bc1ddb3ddc8b483274d9e4065
Nthenya19 <nthenyam@students.uonbi.ac.ke>
2021-12-13 01:53:07 +0300

    Update

--- snap_dead/main.py
+++ snap_dead/main.py
@@ -1,8 +1,10 @@
 from cv2 import cv2
 import cvzone
 cap = cv2.VideoCapture(0)
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 while True:
     _, frame = cap.read()
+    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Snap Dude", frame)
     if cv2.waitkey(10) == ord("q"):
         break
