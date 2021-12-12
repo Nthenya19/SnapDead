fd4fb4d30ae9f8655fd0861acb12105e03ca9200
Nthenya19 <nthenyam@students.uonbi.ac.ke>
2021-12-13 01:56:36 +0300

    Update

--- snap_dead/main.py
+++ snap_dead/main.py
@@ -5,6 +5,9 @@ cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 while True:
     _, frame = cap.read()
     gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray_scale)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow("Snap Dude", frame)
     if cv2.waitkey(10) == ord("q"):
         break
